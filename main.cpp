f0397296248cb863c4fba60f3e336217b6c5da86
kevindotklein <kevindotklein@gmail.com>
2026-05-27 23:37:47 -0300

    v1

---
+++ main.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+#include <iomanip>
+#include "layer.hpp"
+#include "nn.hpp"
+#include "csv.hpp"
+#include "log.hpp"
+#include <sstream>
+
+using namespace std;
+
+int main(){
+
+    try{
+        vector<Layer> layers;
+
+        
+        layers.emplace_back(0,24,ActivationType::NONE);
+        layers.emplace_back(1, 12, ActivationType::RELU);
+        layers.emplace_back(2, 1, ActivationType::SIGMOID);
+
+        NeuralNetwork nn(layers);
+
+        vector<vector<double>> trainingFeatures;
+        vector<vector<double>> trainingLabels;
+        vector<int> training_ids;
+
+        vector<vector<double>> test_features;
+        vector<vector<double>> test_labels;
+        vector<int> test_ids;
+
+        string load_model;
+
+        if (CSV::loadAndSplitSensorData("sensor_readings_24.csv",
+                                    trainingFeatures, trainingLabels, training_ids,
+                                    test_features, test_labels, test_ids,0.8)) {
+            
+            cout << "Deseja carregar o modelo? (s/n): ";
+            cin >> load_model;
+            if (load_model == "y" || load_model == "Y") {
+                cout << "carregando modelo...\n";
+                nn.loadModel();
+            } else {
+                load_model = "n";
+                nn.train(trainingFeatures, trainingLabels, 0.029, 1300,8);
+            }                          
+            
+
+        }else{
+            cerr << "Failed to load sensor data\n";
+            return 1; 
+        }
+        
+        
+        cout << "-------------------Predicoes--------------------\n";
+        L::log("-------------------Predicoes--------------------\n");
+        for (size_t i = 0; i < test_features.size(); ++i) {
+            stringstream data;
+            data << "-------------------Predicao["<<i<<"]--------------------\n"; 
+            data << "Row ID     : " << test_ids[i] << "\n";
+
+            data << "Dados: ";
+            for (double val : test_features[i]) {
+                data << fixed << setprecision(2) << val << " ";
+            }
+            data << "\n";
+
+            vector<double> prediction = nn.predict(test_features[i]);
+
+            data << "Predicao: " << fixed << setprecision(4) << prediction[0] << "\n";
+            data << "Verdadeiro:     : " << test_labels[i][0] << "\n";
+
+            cout << data.str();
+            L::log(data.str());
+        }
+
+        int tp = 0, tn = 0, fp = 0, fn = 0;
+
+        for (size_t i = 0; i < test_features.size(); ++i) {
+            double pred = nn.predict(test_features[i])[0];
+            int predicted = pred >= 0.5 ? 1 : 0;
+            int actual = static_cast<int>(test_labels[i][0]);
+
+            if (predicted == 1 && actual == 1) tp++;
+            else if (predicted == 0 && actual == 0) tn++;
+            else if (predicted == 1 && actual == 0) fp++;
+            else if (predicted == 0 && actual == 1) fn++;
+        }
+
+        int total = tp + tn + fp + fn;
+        double accuracy = static_cast<double>(tp + tn) / total;
+
+        stringstream data;
+        data << "-------------------Metricas---------------------\n";
+        data << "\nConfusion Matrix:\n";
+        data << "TP: " << tp << " | FP: " << fp << "\n";
+        data << "FN: " << fn << " | TN: " << tn << "\n";
+
+        data << std::fixed << std::setprecision(4);
+        data << "\nAcuracia : " << accuracy * 100 << "%\n";
+        cout << data.str();
+        L::log(data.str());
+
+        if(load_model == "n"){
+            string save_model;
+            cout << "Deseja salvar o modelo? (y/n): ";
+            cin >> save_model;
+            if (save_model == "y" || save_model == "Y") {
+                cout << "Salvando o modelo...\n";
+                nn.saveModel();
+            } else {
+                cout << "Model not saved.\n";
+            }
+        }
+        
+    }catch(const exception &e){
+        cerr << " ERROR : " << e.what() << endl;
+        return 1;
+    }
+
+    return 0;
+}
--- nn.hpp
+++ nn.hpp
@@ -5,8 +5,10 @@
 #include <stdexcept>
 #include <cmath>
 #include <chrono>
+#include <sstream>
 #include "layer.hpp"
 #include "matrix.hpp"
+#include "log.hpp"
 
 class NeuralNetwork {
   private:
@@ -61,6 +63,203 @@ class NeuralNetwork {
       std::vector<double> predict(const std::vector<double>& input) {
         return this->foward(input);
       }
+
+      void train(const std::vector<std::vector<double>>& inputs,
+                 const std::vector<std::vector<double>>& targets,
+                 double learningRate,
+                 size_t epochs,
+                 size_t batchSize = 1,
+                 bool verbose = true) {
+                
+          if(inputs.size() != targets.size()) {
+              throw std::runtime_error("Input and target sizes don't match");
+          }
+        
+          if(learningRate <= 0 || epochs <= 0 || batchSize <= 0) {
+              throw std::runtime_error("Learning rate, epochs and batch size must be positive");
+          }
+        
+          size_t datasetSize = inputs.size();
+          auto start = std::chrono::high_resolution_clock::now();
+          double totalError = 0.;
+          double baseLR = learningRate;
+          double decayLR = 0.996; //0.4% por epoca
+          double minLR = 1e-4;
+        
+          if(verbose) {
+              std::stringstream data;
+              data << "--------------------- dados do treinamento -------------------\n";
+              data << "taxa de aprendizado = " << learningRate
+                   << "\t taxa de decaimento = "<<(1.0 - decayLR)*100<<"%"
+                   << "\t EPOCAS = " << epochs <<"\n";
+          
+              data << "tamanho dos dados = " << datasetSize 
+                   << "\t tamanho do batch = " << batchSize << "\n";
+              data << "\n------------------------ treinando ... -----------------------\n";            
+              std::cout << data.str();
+              L::log(data.str());
+          }
+        
+          for(int e=0; e<epochs; e++) {
+              learningRate = std::max(minLR, baseLR * std::pow(decayLR, e)); // diminui com as epocas
+              totalError = 0.;
+          
+              for(size_t b=0; b<datasetSize; b+=batchSize) {
+                  size_t actualBatchSize = std::min(batchSize, (datasetSize - b));
+              
+                  std::vector<Matrix> weightBGrads;
+                  for(size_t i=0; i<this->weights.size(); i++) {
+                      weightBGrads.emplace_back(this->weights[i].getRows(), this->weights[i].getCols());
+                  }
+                
+                  std::vector<std::vector<double>> biasBGrads(this->layers.size() - 1);
+                  for(size_t i=0; i<biasBGrads.size(); i++) {
+                      biasBGrads[i].resize(this->layers[i+1].size, 0.);
+                  }
+                
+                  for(size_t k = b; k < (b + actualBatchSize); k++) {
+                      foward(inputs[k]);
+                      Layer& outputLayer = this->layers.back();
+                  
+                      const double eps = 1e-7;
+                      for(size_t i=0; i<outputLayer.size; i++) {
+                          double yTrue = targets[k][i];
+                          double yPred = outputLayer.after[i];
+                      
+                          // como n tem log(0) precisamos disso
+                          yPred = std::min(std::max(yPred, eps), 1.0 - eps);
+                      
+                          // binary cross-entropy loss (BCE)
+                          totalError += - (yTrue * std::log(yPred) + (1.0 - yTrue) * std::log(1.0 - yPred));
+                      
+                          outputLayer.gradient[i] = yPred - yTrue;
+                      }
+                    
+                      for (int l = (static_cast<int>(this->layers.size()) - 2); l > 0; l--) {
+                          for (size_t i = 0; i < this->weights[l].getRows(); ++i) {
+                              double error = 0.;
+                              for (size_t j = 0; j < this->weights[l].getCols(); ++j) {
+                                  error += this->layers[l+1].gradient[j] * this->weights[l](i, j);
+                              }
+                              layers[l].gradient[i] = error * layers[l].applyDActivation(layers[l].before[i]);
+                          }
+                      }
+                    
+                      for (int l = (static_cast<int>(layers.size()) - 2); l >= 0; l--) {
+                          const std::vector<double>& activations = (l == 0) ? inputs[k] : layers[l].after;
+                      
+                          for (size_t i = 0; i < weightBGrads[l].getRows(); ++i) {
+                              for (size_t j = 0; j < weightBGrads[l].getCols(); ++j) {
+                                  weightBGrads[l](i, j) += layers[l+1].gradient[j] * activations[i];
+                              }
+                          }
+                        
+                          for (size_t i = 0; i < layers[l+1].size; ++i) {
+                              biasBGrads[l][i] += layers[l+1].gradient[i];
+                          }
+                      }
+                  }
+                
+                  for (int l = (static_cast<int>(layers.size()) - 2); l >= 0; l--) {
+                      for (size_t i = 0; i < weights[l].getRows(); ++i) {
+                          for (size_t j = 0; j < weights[l].getCols(); ++j) {
+                              weights[l](i, j) -= learningRate * (weightBGrads[l](i,j) / actualBatchSize);
+                          }
+                      }
+                    
+                      for (size_t i = 0; i < layers[l+1].size; ++i) {
+                          layers[l+1].bias[i] -= learningRate * (biasBGrads[l][i] / actualBatchSize);
+                      }
+                  }
+              }
+            
+              if(verbose && e % 100 == 0) {
+                  std::stringstream data;
+                  data << "["
+                       << (100 * e / epochs) << "%] EPOCA : " << e
+                       << " | BCE: " << totalError / inputs.size() << "\n";
+                  std::cout << data.str();
+                  L::log(data.str());
+              }
+          }
+        
+          auto end = std::chrono::high_resolution_clock::now();
+          if(verbose) {
+              std::stringstream data;
+              data << "------------------- treino completo ---------------------\n";
+              data << "BCE final : " << totalError / inputs.size() << "\n";
+              data << "Tempo de treino : "
+                   << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
+                   << " ms\n";
+              std::cout << data.str();
+              L::log(data.str());
+          }
+      }
+
+       void saveModel(const std::string& filename = "model.csv") const {
+            std::ofstream file(filename);
+            if (!file.is_open()) {
+                std::cerr << "Unable to open file: " << filename << std::endl;
+                return;
+            }
+
+            file << "type,layer,row,col,value\n"; 
+
+            for (size_t l = 1; l < layers.size();++l)
+            {
+                for (size_t i = 0; i < weights[l-1].getRows();++i)
+                {
+                    for (size_t j = 0; j < weights[l-1].getCols();++j)
+                    {
+                        file << "weight," << l << "," << i << "," << j << "," <<  weights[l-1](i,j) << "\n";
+                    }
+                }
+
+                for (size_t i = 0; i < layers[l].bias.size();++i)
+                {
+                    file << "bias," << l << "," << i << ",0," << layers[l].bias[i] << "\n";
+                }
+            }
+
+            file.close();
+            std::cout << "Model saved to " << filename << std::endl;
+        }
+
+        void loadModel(const std::string& filename = "model.csv") {
+            std::ifstream file(filename);
+            if (!file.is_open()) {
+                std::cerr << "Unable to open model file: " << filename << std::endl;
+                return;
+            }
+
+            std::string line;
+            std::getline(file, line);
+
+            while (std::getline(file, line)) {
+                std::stringstream ss(line);
+                std::string type, layerStr, rowStr, colStr, valueStr;
+
+                std::getline(ss, type, ',');
+                std::getline(ss, layerStr, ',');
+                std::getline(ss, rowStr, ',');
+                std::getline(ss, colStr, ',');
+                std::getline(ss, valueStr, ',');
+
+                int layer = std::stoi(layerStr);
+                int row = std::stoi(rowStr);
+                int col = std::stoi(colStr);
+                double value = std::stod(valueStr);
+
+                if (type == "weight") {
+                    weights[layer-1](row, col) = value;
+                } else if (type == "bias") {
+                    layers[layer].bias[row] = value;
+                }
+            }
+
+            file.close();
+            std::cout << "Model loaded from " << filename << std::endl;
+        }
 };
 
 #endif
